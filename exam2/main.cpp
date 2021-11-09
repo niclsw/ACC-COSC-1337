9ca2d7bc970f595e42f2c3f6e28af0925b90eb7e
niclsw <nicklswx@gmail.com>
2021-11-09 10:42:42 -0600

    update 11.9.21

---
+++ exam2/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+struct Animal {
+        string type;
+        string color;
+        int offspring;
+    };
+
+int main() {
+
+    struct Animal animalaccess;
+
+    return 0;
+}
